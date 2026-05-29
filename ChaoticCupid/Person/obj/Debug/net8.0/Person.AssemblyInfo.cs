49240b39c243555868b92e4b1d57e792cc8cd6e5
Ana Paroski <143261039+anaparoskii@users.noreply.github.com>
2026-05-29 18:34:04 +0200

    change gitignore

--- ChaoticCupid/Person/obj/Debug/net8.0/Person.AssemblyInfo.cs
+++ ChaoticCupid/Person/obj/Debug/net8.0/Person.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Person")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bffc4d1c5756796778a3b171d8a1721f8e199e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97fe883e2ef51f287c4eae3197eadc586a6afdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Person")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Person")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
